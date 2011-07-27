eaa874141db012260d66a89d02e63eccdfcd47bc
samlin <samlinzhang@gmail.com>
2011-07-27 20:59:27 +0800

    remove the lxClassDaoImpl to package impl

--- src/net/shopxx/dao/impl/LxClassDaoImpl.java
+++ src/net/shopxx/dao/impl/LxClassDaoImpl.java
@@ -1,8 +1,8 @@
-package net.shopxx.dao;
+package net.shopxx.dao.impl;
 
 import org.springframework.stereotype.Repository;
 
-import net.shopxx.dao.impl.BaseDaoImpl;
+import net.shopxx.dao.LxClassDao;
 import net.shopxx.entity.LxClass;
 @Repository
 public class LxClassDaoImpl extends BaseDaoImpl<LxClass, String> implements LxClassDao {
