731c1f7f35eee2ce93282b0d5e3fc9481fcb2ad2
samlin <samlinzhang@gmail.com>
2011-08-01 22:17:39 +0800

    remove the security check

--- src/com/lxit/common/AdminSecurityListener.java
+++ src/com/lxit/common/AdminSecurityListener.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 
-
-import org.apache.commons.lang.StringUtils;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.Authentication;
@@ -38,55 +36,58 @@ import com.lxit.util.SystemConfigUtil;
 @Transactional
 public class AdminSecurityListener implements ApplicationListener {
 
-	@Resource
-	private AdminService adminService;
-	@Resource
-	private ServletContext servletContext;
+    @Resource
+    private AdminService adminService;
+    @Resource
+    private ServletContext servletContext;
 
-	public void onApplicationEvent(ApplicationEvent event) {
-		
-		String k = (String) servletContext.getAttribute("S" + "H" + "O" + "P" + "X" + "X" + "_" + "K" + "E" + "Y");
-		if (!StringUtils.containsIgnoreCase(k, "s" + "h" + "o" + "p" + "x" + "x")) {
-			throw new RuntimeException();
-		}
-		
-		// 登录成功：记录登录IP、清除登录失败次数
-		if (event instanceof AuthenticationSuccessEvent) {
-			AuthenticationSuccessEvent authEvent = (AuthenticationSuccessEvent) event;
-			Authentication authentication = (Authentication) authEvent.getSource();
-			String loginIp = ((WebAuthenticationDetails)authentication.getDetails()).getRemoteAddress();
-			Admin admin = (Admin) authentication.getPrincipal();
-			admin.setLoginIp(loginIp);
-			admin.setLoginDate(new Date());
-			SystemConfig systemConfig = SystemConfigUtil.getSystemConfig();
-			if (systemConfig.getIsLoginFailureLock() == false) {
-				return;
-			}
-			admin.setLoginFailureCount(0);
-			adminService.update(admin);
-		}
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
 
-		// 登录失败：增加登录失败次数
-		if (event instanceof AuthenticationFailureBadCredentialsEvent) {
-			AuthenticationFailureBadCredentialsEvent authEvent = (AuthenticationFailureBadCredentialsEvent) event;
-			Authentication authentication = (Authentication) authEvent.getSource();
-			String loginUsername = authentication.getName();
-			SystemConfig systemConfig = SystemConfigUtil.getSystemConfig();
-			if (systemConfig.getIsLoginFailureLock() == false) {
-				return;
-			}
-			Admin admin = adminService.get("username", loginUsername);
-			if (admin != null) {
-				int loginFailureCount = admin.getLoginFailureCount() + 1;
-				if (loginFailureCount >= systemConfig.getLoginFailureLockCount()) {
-					admin.setIsAccountLocked(true);
-					admin.setLockedDate(new Date());
-				}
-				admin.setLoginFailureCount(loginFailureCount);
-				adminService.update(admin);
-			}
-		}
+        // String k = (String) servletContext.getAttribute("S" + "H" + "O" + "P"
+        // + "X" + "X" + "_" + "K" + "E" + "Y");
+        // if (!StringUtils.containsIgnoreCase(k, "s" + "h" + "o" + "p" + "x" +
+        // "x")) {
+        // throw new RuntimeException();
+        // }
 
-	}
+        // 登录成功：记录登录IP、清除登录失败次数
+        if (event instanceof AuthenticationSuccessEvent) {
+            AuthenticationSuccessEvent authEvent = (AuthenticationSuccessEvent) event;
+            Authentication authentication = (Authentication) authEvent.getSource();
+            String loginIp = ((WebAuthenticationDetails) authentication.getDetails()).getRemoteAddress();
+            Admin admin = (Admin) authentication.getPrincipal();
+            admin.setLoginIp(loginIp);
+            admin.setLoginDate(new Date());
+            SystemConfig systemConfig = SystemConfigUtil.getSystemConfig();
+            if (systemConfig.getIsLoginFailureLock() == false) {
+                return;
+            }
+            admin.setLoginFailureCount(0);
+            adminService.update(admin);
+        }
+
+        // 登录失败：增加登录失败次数
+        if (event instanceof AuthenticationFailureBadCredentialsEvent) {
+            AuthenticationFailureBadCredentialsEvent authEvent = (AuthenticationFailureBadCredentialsEvent) event;
+            Authentication authentication = (Authentication) authEvent.getSource();
+            String loginUsername = authentication.getName();
+            SystemConfig systemConfig = SystemConfigUtil.getSystemConfig();
+            if (systemConfig.getIsLoginFailureLock() == false) {
+                return;
+            }
+            Admin admin = adminService.get("username", loginUsername);
+            if (admin != null) {
+                int loginFailureCount = admin.getLoginFailureCount() + 1;
+                if (loginFailureCount >= systemConfig.getLoginFailureLockCount()) {
+                    admin.setIsAccountLocked(true);
+                    admin.setLockedDate(new Date());
+                }
+                admin.setLoginFailureCount(loginFailureCount);
+                adminService.update(admin);
+            }
+        }
+
+    }
 
 }
