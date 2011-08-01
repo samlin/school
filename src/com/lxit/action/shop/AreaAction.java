f647ed4977af4095679ba4086add368174241b81
samlin <samlinzhang@gmail.com>
2011-08-01 22:20:35 +0800

    remove the reivision

--- src/com/lxit/action/shop/AreaAction.java
+++ src/com/lxit/action/shop/AreaAction.java
@@ -15,50 +15,37 @@ import org.apache.struts2.convention.annotation.ParentPackage;
 import com.lxit.entity.Area;
 import com.lxit.service.AreaService;
 
-/**
- * 后台Action类 - 地区
- * ============================================================================
- * 版权所有 2008-2010 长沙鼎诚软件有限公司，并保留所有权利。
- * ----------------------------------------------------------------------------
- * 提示：在未取得SHOP++商业授权之前，您不能将本软件应用于商业用途，否则SHOP++将保留追究的权力。
- * ----------------------------------------------------------------------------
- * 官方网站：http://www.shopxx.net
- * ----------------------------------------------------------------------------
- * KEY: SHOPXXAFBAC38E9CCED8343FEA91C4E57AB4B7
- * ============================================================================
- */
-
 @ParentPackage("shop")
 public class AreaAction extends BaseShopAction {
 
-	private static final long serialVersionUID = 1321099291073671591L;
+    private static final long serialVersionUID = 1321099291073671591L;
 
-	@Resource
-	private AreaService areaService;
+    @Resource
+    private AreaService areaService;
 
-	// 根据地区Path值获取下级地区JSON数据
-	public String ajaxChildrenArea() {
-		String path = getParameter("path");
-		if (StringUtils.contains(path,  Area.PATH_SEPARATOR)) {
-			id = StringUtils.substringAfterLast(path, Area.PATH_SEPARATOR);
-		} else {
-			id = path;
-		}
-		List<Area> childrenAreaList = new ArrayList<Area>();
-		if (StringUtils.isEmpty(id)) {
-			childrenAreaList = areaService.getRootAreaList();
-		} else {
-			childrenAreaList = new ArrayList<Area>(areaService.load(id).getChildren());
-		}
-		List<Map<String, String>> optionList = new ArrayList<Map<String, String>>();
-		for (Area area : childrenAreaList) {
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("title", area.getName());
-			map.put("value", area.getPath());
-			optionList.add(map);
-		}
-		JSONArray jsonArray = JSONArray.fromObject(optionList);
-		return ajaxJson(jsonArray.toString());
-	}
+    // 根据地区Path值获取下级地区JSON数据
+    public String ajaxChildrenArea() {
+        String path = getParameter("path");
+        if (StringUtils.contains(path, Area.PATH_SEPARATOR)) {
+            id = StringUtils.substringAfterLast(path, Area.PATH_SEPARATOR);
+        } else {
+            id = path;
+        }
+        List<Area> childrenAreaList = new ArrayList<Area>();
+        if (StringUtils.isEmpty(id)) {
+            childrenAreaList = areaService.getRootAreaList();
+        } else {
+            childrenAreaList = new ArrayList<Area>(areaService.load(id).getChildren());
+        }
+        List<Map<String, String>> optionList = new ArrayList<Map<String, String>>();
+        for (Area area : childrenAreaList) {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("title", area.getName());
+            map.put("value", area.getPath());
+            optionList.add(map);
+        }
+        JSONArray jsonArray = JSONArray.fromObject(optionList);
+        return ajaxJson(jsonArray.toString());
+    }
 
 }
--- src/com/lxit/action/shop/MemberCenterAction.java
+++ src/com/lxit/action/shop/MemberCenterAction.java
@@ -2,7 +2,6 @@ package com.lxit.action.shop;
 
 import javax.annotation.Resource;
 
-
 import org.apache.struts2.convention.annotation.ParentPackage;
 
 import com.lxit.service.MemberService;
@@ -10,35 +9,26 @@ import com.lxit.service.MessageService;
 
 /**
  * 前台Action类 - 会员中心
- * ============================================================================
- * 版权所有 2008-2010 长沙鼎诚软件有限公司，并保留所有权利。
- * ----------------------------------------------------------------------------
- * 提示：在未取得SHOP++商业授权之前，您不能将本软件应用于商业用途，否则SHOP++将保留追究的权力。
- * ----------------------------------------------------------------------------
- * 官方网站：http://www.shopxx.net
- * ----------------------------------------------------------------------------
- * KEY: SHOPXX456CA553F200C7A4CFEC6788B8F51192
- * ============================================================================
  */
 
 @ParentPackage("member")
 public class MemberCenterAction extends BaseShopAction {
 
-	private static final long serialVersionUID = -3568504222758246021L;
-	
-	@Resource
-	MemberService memberService;
-	@Resource
-	MessageService messageService;
-	
-	// 会员中心首页
-	public String index() {
-		return "index";
-	}
+    private static final long serialVersionUID = -3568504222758246021L;
 
-	// 获取未读消息数量
-	public Long getUnreadMessageCount() {
-		return messageService.getUnreadMessageCount(getLoginMember());
-	}
-	
+    @Resource
+    MemberService memberService;
+    @Resource
+    MessageService messageService;
+
+    // 会员中心首页
+    public String index() {
+        return "index";
+    }
+
+    // 获取未读消息数量
+    public Long getUnreadMessageCount() {
+        return messageService.getUnreadMessageCount(getLoginMember());
+    }
+
 }
